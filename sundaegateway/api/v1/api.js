cc3f83f441a1941decf85e0c81b495cb8008e0eb
elbanby <obanby@gmail.com>
2019-06-22 03:00:12 -0400

    Add twilio itegration

--- sundaegateway/api/v1/api.js
+++ sundaegateway/api/v1/api.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const sms = require("../../twilio/sms");
 
 const api = express.Router();
+api.use(bodyParser.urlencoded({ extended: false }));
 
 api.get("/health", (req, res) => {
+  sms.send("Hi there");
   res.end("sundae API status [up]\n")
 });
 
-
+api.post("/sms", (req, res) => {
+  var addOn = JSON.parse(req.body.AddOns);
+  const sentimentScore = addOn.results.marchex_sentiment.result.result;
+  const twiml = sms.messageResponse();
+  twiml.message(req.body.Body);
+  res.writeHead(200, {'Content-Type': 'text/xml'})
+  res.end(twiml.toString());
+});
 
 module.exports = api;
---
+++ sundaegateway/twilio/sms.js
@@ -0,0 +1,26 @@
+const accountSid = process.env.TWILIO_ACCOUNTSID;
+const authToken  = process.env.TWILIO_AUTHTOKEN;
+
+const client = require("twilio")(accountSid, authToken);
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
+
+async function send(msg) {
+  try {
+    const msgId  = await client.messages.create({
+        to: ,
+        from: "+16474902192",
+        body: msg
+      });
+  } catch(err) {
+    console.log(`error occured ${err}`)
+  }
+}
+
+function messageResponse() {
+  return  (new MessagingResponse);
+}
+
+module.exports = {
+  send: send,
+  messageResponse: messageResponse
+}
